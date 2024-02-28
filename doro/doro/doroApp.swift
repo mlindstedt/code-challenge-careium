569c187989ec16447d306f0330979bceb941b6f7
Magnus Lindstedt <mlindst@icloud.com>
2024-02-28 13:41:45 +0100

    Empty Swift app with simple UI added

---
+++ doro/doro/doroApp.swift
@@ -0,0 +1,17 @@
+//
+//  doroApp.swift
+//  doro
+//
+//  Created by Magnus Lindstedt on 2024-02-28.
+//
+
+import SwiftUI
+
+@main
+struct doroApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
